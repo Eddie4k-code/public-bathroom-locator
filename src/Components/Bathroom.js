ce33e6c04978e91f450b99d335b1e4c040f2b81b
Eddie4k-code <98666468+Eddie4k-code@users.noreply.github.com>
2023-02-22 11:30:19 -0500

    Changed styling and added Copy Address function

--- src/Components/Bathroom.js
+++ src/Components/Bathroom.js
@@ -1,19 +1,33 @@
+import { useState } from "react";
+
 export const Bathroom = ({ name, street, city, miles, comment, directions }) => {
+
+
+    const [copied, setCopied] = useState(false);
+
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(`${street}`);
+        setCopied(true);
+    };
 
     return (
         
-     <div className="p-4 border-t border-gray-200 flex flex-col md:flex-row md:justify-between">
-  <div>
-    <p className="text-lg font-medium">{name}</p>
-    {miles && <p className="text-gray-500 font-medium">{miles} miles away</p>}
-  </div>
-  <div className="md:pl-4">
-    {street && <p className="text-gray-500 font-medium">Street: {street}</p>}
-    {city && <p className="text-gray-500 font-medium">City: {city}</p>}
-    {comment && <p className="text-gray-500 font-medium">Comment: {comment}</p>}
-    {directions && <p className="text-gray-500 font-medium">Directions: {directions}</p>}
-  </div>
-</div>
+        <div className="bg-white rounded-md shadow-md overflow-hidden">
+           
+            <div className="p-4">
+                <h2 className="text-xl font-medium">{name}</h2>
+                <div className="mt-2 text-gray-700">{street} {city}</div>
+                {miles && <div className="mt-2 text-gray-700">{miles} miles away</div>}
+                {comment && <div className="mt-2 text-gray-700">{comment}</div>}
+                {directions && <div className="mt-2 text-gray-700">{directions}</div>}
+                <button
+                    className="mt-4 py-2 px-4 border border-gray-500 rounded-md text-gray-500 hover:text-white hover:bg-gray-500"
+                    onClick={copyToClipboard}
+                >
+                    {copied ? 'Address Copied!' : 'Copy Address'}
+                </button>
+            </div>
+        </div>
         
         );
 
